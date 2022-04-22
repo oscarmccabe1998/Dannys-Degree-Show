4fd021299fa39f6b4ce5e86164285460e915dc59
oscarmccabe1998 <oscarmccabe98@gmail.com>
2022-04-22 15:50:03 +0100

    query testing

--- order_taking/models.py
+++ order_taking/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 import datetime
 from django.utils import timezone
-   
+
+#admin oscarm pwd dannysdegreeshow
+
 class Order(models.Model):  #class to pass data to database 
     class TableChoice(models.IntegerChoices):   #defining tables for the orders 
         Table1 = 1
@@ -60,7 +62,7 @@ class Order(models.Model):  #class to pass data to database
 
 
     Date = models.DateTimeField(default=datetime.datetime.now)      #all feilds that will be in the database table
-    ref = models.CharField(max_length=200, default="n/a")
+    ref = models.CharField(max_length=200, blank=True)
     table_number = models.IntegerField(choices=TableChoice.choices, default='1')
     starters = models.CharField(max_length=25, choices=STARTER)
     mains = models.CharField(max_length=28, choices=MAIN)
--- order_taking/views.py
+++ order_taking/views.py
@@ -13,15 +13,28 @@ def index(request):
     x = 17
     template = loader.get_template('order_taking/index.html')
     for order in range(x):
-        query = Order.objects.raw("SELECT * FROM order_taking_Order WHERE table_number = %s", [order])
+        query = Order.objects.raw("SELECT * FROM order_taking_Order WHERE table_number = %s AND waiting_for_service = FALSE", [order])
         for q in query:
-            print(q.table_number)
-            print(q.starters)
+            #order_list.append(q)
             starts.append(q.starters)
-        
+            mains.append(q.mains)
+            dessert.append(q.deserts)
+        #for q in query:
+        #    print(q.table_number)
+            #print(q.starters)
+        #    starts.append(q.starters)
+        order_list.append(order)
+    order_list.append(starts)
+    order_list.append(mains)
+    order_list.append(dessert)
+    #print(starts)
+    #print(mains)
+    #print(dessert)
+    print(order_list)
     context = {
         'order_list': order_list
     }
+    #print(order_list)
     return render(request, 'order_taking/index.html', {"order_list": order_list})
     
 
