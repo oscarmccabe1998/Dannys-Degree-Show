17d50462903c2d0b3460f5a4793872cf7c4d474c
oscarmccabe1998 <oscarmccabe98@gmail.com>
2022-04-24 17:42:27 +0100

    grouping error in progress

--- order_taking/models.py
+++ order_taking/models.py
@@ -73,4 +73,77 @@ class Order(models.Model):  #class to pass data to database
         return self.deserts
         return self.starters
 
-#class Check(models.Model):
+class table(models.Model):
+    auto_increment_id = models.AutoField(primary_key=True)
+    class TableChoice(models.IntegerChoices):   #defining tables for the orders 
+        Table1 = 1
+        Table2 = 2
+        Table3 = 3
+        Table4 = 4
+        Table5 = 5
+        Table6 = 6
+        Table7 = 7
+        Table8 = 8
+        Table9 = 9
+        Table10 = 10
+        Table11 = 11
+        Table12 = 12
+        Table13 = 13
+        Table14 = 14
+        Table15 = 15
+        Table16 = 16
+    table_number = models.IntegerField(choices=TableChoice.choices, default='1')
+    Date = models.DateTimeField(default=datetime.datetime.now)
+    ref = models.CharField(max_length=200, blank=True)
+
+class starter(models.Model):
+    STARTER = (
+        ('Soup of the day', 'Soup of the day'),     #Menu options to make data entry on the back end easier 
+        ('Roasted red pepper hummus', 'Roasted red pepper hummus'),
+        ('Charcuterie Board', 'Charcuterie Board'),
+        ('Arbroath Smokie Risotto', 'Arbroath Smokie Risotto'),
+        ('Hot Wings', 'Hot Wings'),
+        ('Duck Leg & Fig Terrine', 'Duck Leg & Fig Terrine'),
+        ('Beet Root Salad', 'Beet Root Salad'),
+    )
+    table_number = models.ForeignKey(table, on_delete=models.CASCADE)
+    starters = models.CharField(max_length=25, choices=STARTER)
+
+class main(models.Model):
+    MAIN = (
+        ('Ribeye Steak', 'Ribeye Steak'),
+        ('Sirloin Steak', 'Sirloin Steak'),
+        ('Cheese Burger', 'Cheese Burger'),
+        ('Black And Blue Burger', 'Black And Blue Burger'),
+        ('Moving Mountain', 'Moving Mountain'),
+        ('The Baxter', 'The Baxter'),
+        ('Miss Daisy', 'Miss Daisy'),
+        ('Beer Battered, Haddock', 'Beer Battered, Haddock'),
+        ('Cod Fillet', 'Cod Fillet'),
+        ('Perthshire Duck Breast', 'Perthshire Duck Breast'),
+        ('Burgundy Beef', 'Burgundy Beef'),
+        ('Mushroom & Tarragon Linguine', 'Mushroom & Tarragon Linguine'),
+        ('Spiced Polenta', 'Spiced Polenta'),
+        ('Mac and Cheese', 'Mac and Cheese'),
+
+    )
+    table_number = models.ForeignKey(table, on_delete=models.CASCADE)
+    mains = models.CharField(max_length=28, choices=MAIN)
+
+class desert(models.Model):
+    DESERT = (
+        ('Bread & Butter Pudding', 'Bread & Butter Pudding'),
+        ('Praline Chocolate Tart', 'Praline Chocolate Tart'),
+        ('Lavender Panna Cotta', 'Lavender Panna Cotta'),
+        ('Sticky Toffee Sundae', 'Sticky Toffee Sundae'),
+        ('Clarckies Ice Cream', 'Clarckies Ice Cream'),
+        ('Cheese Board', 'Cheese Board'),
+    )
+    table_number = models.ForeignKey(table, on_delete=models.CASCADE)
+    deserts = models.CharField(max_length=22, choices=DESERT)
+
+class check(models.Model):
+    table_number =  models.ForeignKey(table, on_delete=models.CASCADE)
+    starter =  models.ForeignKey(starter, on_delete=models.CASCADE)
+    main = models.ForeignKey(main, on_delete=models.CASCADE)
+    desert = models.ForeignKey(desert, on_delete=models.CASCADE)
--- order_taking/views.py
+++ order_taking/views.py
@@ -1,41 +1,96 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
-from .models import Order
+from .models import Order, starter, table
 
 
 def index(request):
     orders = Order.objects.all()
+    teststarter = starter.objects.all()
+    #print(teststarter)
+    #starter_query = starter.objects.raw("SELECT * FROM order_taking_starter WHERE table_number = 1")
+    #test = table.objects.raw("SELECT * FROM order_taking_table WHERE auto_increment_id = 1")
+    #for item in test:
+    #    print(item)
     starts = []
     mains = []
     dessert = []
     order_list = []
+    checkstart = []
+    temp = ""
+    temp1 = ""
+    templist = []
+    #values = " "
     x = 17
     template = loader.get_template('order_taking/index.html')
+    #for order in range(x):
+    #    checkstart = starter.objects.filter(table_number= order)
+    #    print(checkstart)
+    #    for q in checkstart:
+    #        temp = ""
+    #        starts.append(q.starters)
+    #        table = q.table_number.table_number
+    #        temp = temp + q.starters + ", "
     for order in range(x):
-        query = Order.objects.raw("SELECT * FROM order_taking_Order WHERE table_number = %s AND waiting_for_service = FALSE", [order])
-        for q in query:
+        temp = starter.objects.filter(table_number= order)
+        y = len(temp)
+        print(y)
+
+        for test in temp:
+            order_list.append(test.starters)
+        print(order_list)
+    for item in order_list:
+        checkstart.append(item)
+    print(checkstart)
+        #order_list.append(checkstart.starters)
+
+            #tablenum = table.objects.filter(table_number = table)
+            #print(tablenum.table_number)
+    #print(temp)
+    #order1 = []
+    
+    #order1.append(starter.objects.filter(table_number = 1))
+    #order_list.append(order1)
+    #print(order_list)
+    #print(starts)
+
+    
+
+        #query = Order.objects.raw("SELECT * FROM order_taking_Order WHERE table_number = %s AND waiting_for_service = FALSE", [order])
+        #starter_query = Order.objects.raw("SELECT * FROM order_taking_starter WHERE table_number = %s", [order])
+    #    starter_query = starter.objects.raw("SELECT table_number FROM order_taking_starter", translations=order_taking_table)
+    #values=values+starts
+    #print(values)
+    #order_list.append(starts)
+
+    #    for q in starter_query:
+    #        starts.append(q)
             #order_list.append(q)
-            starts.append(q.starters)
-            mains.append(q.mains)
-            dessert.append(q.deserts)
+            #starts.append(q.starters)
+            #mains.append(q.mains)
+            #dessert.append(q.deserts)
         #for q in query:
         #    print(q.table_number)
             #print(q.starters)
         #    starts.append(q.starters)
-        order_list.append(order)
-    order_list.append(starts)
-    order_list.append(mains)
-    order_list.append(dessert)
-    print(starts)
-    print(mains)
-    print(dessert)
+    #    order_list.append(order)
+    #order_list.append(starts)
+    #order_list.append(mains)
+    #order_list.append(dessert)
+
+    #print(values)
+    
+    
+    
     #print(order_list)
     context = {
-        'order_list': order_list
+        'order_list': order_list, 
+        'starts': starts,
+        'table': table,
+        'checkstart': checkstart
     }
     #print(order_list)
-    return render(request, 'order_taking/index.html', {"order_list": order_list})
+    return render(request, 'order_taking/index.html', context)
     
 
 
